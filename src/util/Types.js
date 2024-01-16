78575b20288d770c6da3ab652b70c72a4b51f7cf
BrennanAndruss <brennanandruss@gmail.com>
2024-01-16 09:10:19 -0800

    moved type icons

---
+++ src/util/Types.js
@@ -0,0 +1,41 @@
+import iconBug from "../assets/bug.svg"
+import iconDark from "../assets/dark.svg"
+import iconDragon from "../assets/dragon.svg"
+import iconElectric from "../assets/electric.svg"
+import iconFairy from "../assets/fairy.svg"
+import iconFighting from "../assets/fighting.svg"
+import iconFire from "../assets/fire.svg"
+import iconFlying from "../assets/flying.svg"
+import iconGhost from "../assets/ghost.svg"
+import iconGrass from "../assets/grass.svg"
+import iconGround from "../assets/ground.svg"
+import iconIce from "../assets/ice.svg"
+import iconNormal from "../assets/normal.svg"
+import iconPoison from "../assets/poison.svg"
+import iconPsychic from "../assets/psychic.svg"
+import iconRock from "../assets/rock.svg"
+import iconSteel from "../assets/steel.svg"
+import iconWater from "../assets/water.svg"
+
+const types = {
+  bug: iconBug,
+  dark: iconDark,
+  dragon: iconDragon,
+  electric: iconElectric,
+  fairy: iconFairy,
+  fighting: iconFighting,
+  fire: iconFire,
+  flying: iconFlying,
+  ghost: iconGhost,
+  grass: iconGrass,
+  ground: iconGround,
+  ice: iconIce,
+  normal: iconNormal,
+  poison: iconPoison,
+  psychic: iconPsychic,
+  rock: iconRock,
+  steel: iconSteel,
+  water: iconWater
+}
+
+export default types
